5bb57ee15857665bab291ff537c1952e3edcb5a9
Andre Mendes Marcondes <bistomania@live.com>
2020-10-24 16:43:33 -0300

    rota vurerouter

--- app1/src/main.js
+++ app1/src/main.js
@@ -3,25 +3,36 @@ import Vue from 'vue'
 import Vueresource from 'vue-resource'
 Vue.use(Vueresource)
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
 import Home from './paginas/Home.vue'
 const Contato = { template: '<p>Página de Contato</p>' }
 const SemPagina = { template: '<p>Sem página</p>' }
 
-const rotas = {
-  '/': Home,
-  '/home': Home,
-  '/contato': Contato
-}
+const routes = [
+  { path: '/', component: Home },
+  { path: '/home', component: Home },
+  { path: '/contato', component: Contato }
+]
 
-new Vue({
-  el: '#app',
-  data: {
-    rotaAtual: window.location.pathname
-  },
-  computed: {
-    exibeComponente() {
-      return rotas[this.rotaAtual] || SemPagina;
-    }
-  },
-  render(h) { return h(this.exibeComponente) }
+const router = new VueRouter({
+  routes
 })
+
+const app = new Vue({
+  router
+}).$mount('#app')
+
+//new Vue({
+//  el: '#app',
+//  data: {
+//    rotaAtual: window.location.pathname
+//  },
+//  computed: {
+//    exibeComponente() {
+//      return rotas[this.rotaAtual] || SemPagina;
+//    }
+//  },
+//  render(h) { return h(this.exibeComponente) }
+//})
