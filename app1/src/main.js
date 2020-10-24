c6dbf4ca3f31342a5daf1e31e8fa31c9c8172fb4
Andre Mendes Marcondes <bistomania@live.com>
2020-10-24 16:35:32 -0300

    rota simples

--- app1/src/main.js
+++ app1/src/main.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
 import Vueresource from 'vue-resource'
 Vue.use(Vueresource)
 
+import Home from './paginas/Home.vue'
+const Contato = { template: '<p>Página de Contato</p>' }
+const SemPagina = { template: '<p>Sem página</p>' }
+
+const rotas = {
+  '/': Home,
+  '/home': Home,
+  '/contato': Contato
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data: {
+    rotaAtual: window.location.pathname
+  },
+  computed: {
+    exibeComponente() {
+      return rotas[this.rotaAtual] || SemPagina;
+    }
+  },
+  render(h) { return h(this.exibeComponente) }
 })
